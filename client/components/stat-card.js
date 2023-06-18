70b7f3182a640125ff3a1f4fc9d952843addf75c
prapti koirala <praptikoirala6@gmail.com>
2023-06-18 16:57:17 +0545

    design statistics page

--- client/components/stat-card.js
+++ client/components/stat-card.js
@@ -1,24 +1,20 @@
 const StatCard = ({ title, stat }) => {
-    return (
-        <div className="bg-gray-50 w-fit px-3 py-2 rounded-xl shadow dark-light">
-            <h3 className="text-xl font-semibold capitalize">{title}</h3>
+  return (
+    <div className="bg-gray-50 w-fit px-3 py-2 rounded-xl shadow dark-light">
+      <h3 className="text-xl font-semibold capitalize">{title}</h3>
 
-            <div>
-                {Object.keys(stat).map((prop) => {
-                    return (
-                        <div className="flex flex-col justify-center items-center">
-                            <span className={`text-lg font-semibold`}>
-                                {stat[prop]}
-                            </span>
-                            <span className="-mt-1 text-sm text-gray-one">
-                                {prop}
-                            </span>
-                        </div>
-                    );
-                })}
+      <div className="flex space-x-3">
+        {Object.keys(stat).map((prop) => {
+          return (
+            <div className="flex flex-col justify-center items-center">
+              <span className={`text-lg font-semibold`}>{stat[prop]}</span>
+              <span className="-mt-1 text-sm text-gray-one">{prop}</span>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default StatCard;
