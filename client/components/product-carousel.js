485244b91dbaa0ad97e6f189b7fc517ed95b918a
prapti koirala <praptikoirala6@gmail.com>
2023-06-23 12:53:45 +0545

    change recommendation carousel

--- client/components/product-carousel.js
+++ client/components/product-carousel.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ContentList from "./content-list";
+// import ContentList from "./content-list";
+import ContentCard from "./content-card";
 
 const ProductCarousel = ({ products }) => {
   return (
-    <div className="mt-2 overflow-x-auto scrollbar-hide">
+    <div className="mt-2 flex space-x-5 overflow-x-auto scrollbar-hide">
       {products.length <= 0 && (
         <p className="status">No similar products found...</p>
       )}
 
-      <ContentList list={products} type="product" />
+      {products.map((product) => {
+        return <ContentCard {...product} key={product.id} type="product" />;
+      })}
     </div>
   );
 };
