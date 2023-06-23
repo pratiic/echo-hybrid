7a42af710dfbc8259aec46290866687168326426
prapti koirala <praptikoirala6@gmail.com>
2023-06-23 20:27:55 +0545

    style product recommendation carousel

--- client/components/similar-products.js
+++ client/components/similar-products.js
@@ -1,15 +1,19 @@
 import ProductCarousel from "./product-carousel";
 
 const SimilarProducts = ({ products = [] }) => {
-    return (
-        <div className="-mt-3 mb-5">
-            <h3 className="black-white text-xl font-semibold mb-2">
-                Similar Products
-            </h3>
+  return (
+    <div className="-mt-3 mb-5">
+      <h3 className="black-white text-xl font-semibold mb-2">
+        Similar Products
+      </h3>
 
-            <ProductCarousel products={products} />
-        </div>
-    );
+      {products.length <= 0 && (
+        <p className="dark-light max-w-sm">No similar products found</p>
+      )}
+
+      <ProductCarousel products={products} />
+    </div>
+  );
 };
 
 export default SimilarProducts;
